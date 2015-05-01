dfeeb6b89a940e6b2c456e5fcc1d63e599af0da4
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-04-30 21:19:49 -0400

    Products & Twitter

---
+++ shirts.js
@@ -0,0 +1,70 @@
+var shirt1 = false;
+var shirt2 = false;
+var shirt3 = false;
+var shirt4 = false;
+var shirt5 = false;
+var shirt6 = false;
+
+function AddValue1() {
+	if(shirt1 === false){
+		shirt1 = true;
+}
+	else{
+		shirt1 = false;
+} 
+		alert(shirt1);
+
+}
+
+function AddValue2() {
+        if(shirt2 === false){
+                shirt2 = true;
+}
+        else{ 
+                shirt2 = false;
+}
+                alert(shirt2);
+
+}
+
+function AddValue3() {
+        if(shirt3 === false){
+                shirt3 = true;
+}
+        else{ 
+                shirt3 = false;
+}
+                alert(shirt3);
+
+}
+
+function AddValue4() {
+        if(shirt4 === false){
+                shirt4 = true;
+}
+        else{ 
+                shirt4 = false;
+}
+                alert(shirt4);
+
+}
+
+function AddValue5() {
+        if(shirt5 === false){
+                shirt5 = true;
+}
+        else{ 
+                shirt5 = false;
+}
+                alert(shirt5);
+
+function AddValue6() {
+        if(shirt6 === false){
+                shirt6 = true;
+}
+        else{ 
+                shirt6 = false;
+}
+                alert(shirt6);
+
+}

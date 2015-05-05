3f1614ddb82aef13388dde05c45bccd4324068c4
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-05-05 18:39:08 -0400

    Final products

--- shirts.js
+++ shirts.js
@@ -1,70 +1,76 @@
-var shirt1 = false;
-var shirt2 = false;
-var shirt3 = false;
-var shirt4 = false;
-var shirt5 = false;
-var shirt6 = false;
+var shirt1 = 0;
+var shirt2 = 0;
+var shirt3 = 0;
+var shirt4 = 0;
+var shirt5 = 0;
+var shirt6 = 0;
 
 function AddValue1() {
-	if(shirt1 === false){
-		shirt1 = true;
+	if(shirt1 === 0){
+		shirt1 = 12.99;
 }
 	else{
-		shirt1 = false;
+		shirt1 = 0;
 } 
 		alert(shirt1);
 
 }
 
 function AddValue2() {
-        if(shirt2 === false){
-                shirt2 = true;
+        if(shirt2 === 0){
+                shirt2 = 19.99;
 }
         else{ 
-                shirt2 = false;
+                shirt2 = 0;
 }
                 alert(shirt2);
 
 }
 
 function AddValue3() {
-        if(shirt3 === false){
-                shirt3 = true;
+        if(shirt3 === 0){
+                shirt3 = 17.99;
 }
         else{ 
-                shirt3 = false;
+                shirt3 = 0;
 }
                 alert(shirt3);
 
 }
 
 function AddValue4() {
-        if(shirt4 === false){
-                shirt4 = true;
+        if(shirt4 === 0){
+                shirt4 = 21.99;
 }
         else{ 
-                shirt4 = false;
+                shirt4 = 0;
 }
                 alert(shirt4);
 
 }
 
 function AddValue5() {
-        if(shirt5 === false){
-                shirt5 = true;
+        if(shirt5 === 0){
+                shirt5 = 15.99;
 }
         else{ 
-                shirt5 = false;
+                shirt5 = 0;
 }
                 alert(shirt5);
+}
 
 function AddValue6() {
-        if(shirt6 === false){
-                shirt6 = true;
+        if(shirt6 === 0){
+                shirt6 = 23.99;
 }
         else{ 
-                shirt6 = false;
+                shirt6 = 0;
 }
                 alert(shirt6);
 
 }
+
+function AddTotal() {
+	alert(shirt1 + shirt2 + shirt3 + shirt4 + shirt5 + shirt6);
+}
+
